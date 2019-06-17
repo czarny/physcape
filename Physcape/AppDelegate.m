88b4965405be3828af336538191917dd85c7dcc6
czarny <kczarnota7@gmail.com>
2019-06-17 18:02:36 +0200

    Replaced § key with esc key.

--- Physcape/AppDelegate.m
+++ Physcape/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import <IOKit/hidsystem/IOHIDEventSystemClient.h>
+#import <IOKit/hidsystem/IOHIDServiceClient.h>
+#import <IOKit/hid/IOHIDUsageTables.h>
 
 
 
@@ -29,16 +32,63 @@
     self.statusItem.button.image = [NSImage imageNamed:@"Icon"];
     self.statusItem.button.toolTip = [NSString stringWithFormat:@"Physcape %@", version];
     self.statusItem.menu = self.statusMenu;
+    
+    [self mapESC];
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+    [self unmapESC];
 }
 
 
 - (IBAction)onQuit:(id)sender {
     [[NSApplication sharedApplication] terminate:self];
+}
+
+
+- (void)mapESC {
+    IOHIDEventSystemClientRef system;
+    CFArrayRef services;
+    
+    uint64_t src_key = 0x700000064;     // Keyboard Non-US \ and |
+    uint64_t dst_key = 0x700000029;     // Keyboard Escape
+    
+    NSArray *map = @[
+                     @{@kIOHIDKeyboardModifierMappingSrcKey:@(src_key),
+                       @kIOHIDKeyboardModifierMappingDstKey:@(dst_key)},
+                     ];
+    
+    system = IOHIDEventSystemClientCreateSimpleClient(kCFAllocatorDefault);
+    services = IOHIDEventSystemClientCopyServices(system);
+    for(CFIndex i = 0; i < CFArrayGetCount(services); i++) {
+        IOHIDServiceClientRef service = (IOHIDServiceClientRef)CFArrayGetValueAtIndex(services, i);
+        if(IOHIDServiceClientConformsTo(service, kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard)) {
+            IOHIDServiceClientSetProperty(service, CFSTR(kIOHIDUserKeyUsageMapKey), (CFArrayRef)map);
+        }
+    }
+    
+    CFRelease(services);
+    CFRelease(system);
+}
+
+
+- (void)unmapESC {
+    IOHIDEventSystemClientRef system;
+    CFArrayRef services;
+    
+    system = IOHIDEventSystemClientCreateSimpleClient(kCFAllocatorDefault);
+    services = IOHIDEventSystemClientCopyServices(system);
+    for(CFIndex i = 0; i < CFArrayGetCount(services); i++) {
+        IOHIDServiceClientRef service = (IOHIDServiceClientRef)CFArrayGetValueAtIndex(services, i);
+        if(IOHIDServiceClientConformsTo(service, kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard)) {
+            IOHIDServiceClientSetProperty(service, CFSTR(kIOHIDUserKeyUsageMapKey), (CFArrayRef)@[]);
+        }
+    }
+    
+    CFRelease(services);
+    CFRelease(system);
 }
 
 
